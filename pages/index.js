61993398ab933c7122d854258df5bd4c82dd7193
nathaliachagas2510-ux <nathaliachagas2510@gmail.com>
2026-03-12 22:32:06 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-    return <h1>teste</h1>Asafe, eu amo voce. Se voce me ama , da uma risada!
+    return (
+  <div> 
+    <h1>teste</h1>
+    <p>Asafe,eu amo voce. Se voce me ama, Da uma risada!!</p>
+  </div>      
+)
 }
 
 export default Home;
+
+
 
